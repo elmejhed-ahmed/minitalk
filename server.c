efc56d2f0f00a7a23f536187d498d7f82fbce33f
elmejhed-ahmed <ahmedelmejhed11@gmail.com>
2024-04-01 17:14:59 +0000

    minitalk

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: ael-mejh <ael-mejh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 00:36:24 by ael-mejh          #+#    #+#             */
-/*   Updated: 2024/04/01 01:15:01 by ael-mejh         ###   ########.fr       */
+/*   Updated: 2024/04/01 17:14:37 by ael-mejh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,22 @@
 
 void ft_handler(int sig, siginfo_t *info, void *context) 
 {
-    // static char buffer;
-    // static int i;
-    printf("signal %d ///// PID : %d", sig, info->si_pid);
-    
-    // int res;
-    // res = 0;
-    // if (sig == SIGUSR1)
-    // {
-    //     printf("dddd");
-    //     //res = (0 << i & 1);
-    //     buffer = res;
-    // }
-        
-    // if (i == 8)
-    // {
-    //     printf("%c", buffer);
+    static char bit;
+    static int i;
 
-    //     i = 0;
-    // }
-    // i++;
+    i++;
+    if (sig == SIGUSR1)
+        printf("user1-->%d\n",  bit|= (0 << i));
+    if (sig == SIGUSR2)
+        printf("user2-->%d\n", bit |= (1 << i));
+    
+    if (i == 8)
+    {
+        printf("%d\n", bit);
+        i = 0;
+        bit = 0;
+    }
+
 }
 
 int main(int ac, char **av)
@@ -45,8 +41,10 @@ int main(int ac, char **av)
     sig.sa_sigaction = ft_handler;
     sig.sa_flags = SA_SIGINFO;
 
-    if(ac > 1)
-        return (write(2, "Error\n", 6), 1);
+    if (ac > 1) {
+        write(2, "Error\n", 6);
+        return 1;
+    }
     
     printf("PID: %d\n", getpid());
     
@@ -54,17 +52,17 @@ int main(int ac, char **av)
         perror("sigaction");
         return 1;
     }
-    
+
     if (sigaction(SIGUSR2, &sig, NULL) == -1) {
         perror("sigaction");
         return 1;
     }
     
     while (1) {
-        sleep(1);
+        pause();
     }
-    https://www.phind.com/search?cache=u9awpp48dwbz9e9z6mdd3dca
-    // return 0;
+    
+    return 0;
 }
 
 
